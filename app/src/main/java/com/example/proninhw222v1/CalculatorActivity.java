d94bbe6700da11fae8090e2d732e7a904f47fa54
Makkurano <makkurano@gmail.com>
2019-05-29 13:36:31 +0300

    Test

---
+++ app/src/main/java/com/example/proninhw222v1/CalculatorActivity.java
@@ -0,0 +1,4 @@
+package com.example.proninhw222v1;
+
+public class CalculatorActivity {
+}
--- app/src/main/java/com/example/proninhw222v1/MainActivity.java
+++ app/src/main/java/com/example/proninhw222v1/MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.proninhw222v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -23,10 +22,18 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_open_notes) {
-            Toast.makeText(MainActivity.this, "Отркыть записную книжку", Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+            Toast.makeText(MainActivity.this, "Отркываю записную книжку", Toast.LENGTH_LONG).show();
             return true;
         }
-
+        if (id == R.id.action_settings) {
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+            Toast.makeText(MainActivity.this, "Отркываю калькулятор", Toast.LENGTH_LONG).show();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+
     }
 }
