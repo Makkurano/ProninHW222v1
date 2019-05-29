a63761b9ff1a7b2c6d82b4133f6f6e8b5901cf11
Makkurano <makkurano@gmail.com>
2019-05-29 12:19:12 +0300

    Test

--- app/src/main/java/com/example/proninhw222v1/MainActivity.java
+++ app/src/main/java/com/example/proninhw222v1/MainActivity.java
@@ -29,10 +29,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
