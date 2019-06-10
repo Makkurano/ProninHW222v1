2bd445d66e8eabaf51adcbb607b01003c49b567e
Makkurano <makkurano@gmail.com>
2019-06-10 17:07:45 +0300

    Done

--- app/src/main/java/com/example/proninhw222v1/MainActivity.java
+++ app/src/main/java/com/example/proninhw222v1/MainActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+//import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
